fef5216058b2cec463e4c9c6bc0d661b9dd6acef
Sebastian Reimers <hallo@studio-link.de>
2022-12-06 20:31:35 +0100

    remove cancel button

--- webui/src/components/SettingsModal.vue
+++ webui/src/components/SettingsModal.vue
@@ -112,14 +112,6 @@
                 >
                   Join the conversation
                 </button>
-                <button
-                  type="button"
-                  class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  @click="open = false"
-                  ref="cancelButtonRef"
-                >
-                  Cancel
-                </button>
               </div>
             </DialogPanel>
           </TransitionChild>
