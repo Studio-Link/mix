519bb4714dcc2b1992ffc25df736eaffc331ab31
Sebastian Reimers <hallo@studio-link.de>
2023-01-08 08:37:47 +0100

    bdd: add port check

--- tests/bdd/environment.py
+++ tests/bdd/environment.py
@@ -1,5 +1,26 @@
 import subprocess
 import time
+import socket
+
+
+def is_port_reachable(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, port))
+        return True
+    except Exception:
+        return False
+    finally:
+        s.close()
+
+
+def wait_until_port_reachable(host, port, timeout):
+    start_time = time.time()
+    while not is_port_reachable(host, port):
+        if time.time() - start_time > timeout:
+            raise TimeoutError(
+                'Timed out waiting for port to become reachable')
+        time.sleep(0.01)
 
 
 def before_all(context):
@@ -8,7 +29,7 @@ def before_all(context):
     context.sessid = {}
     context.response = {}
     context.proc = subprocess.Popen(["./build/slmix"], cwd="../../.")
-    time.sleep(0.2)
+    wait_until_port_reachable('127.0.0.1', 9999, 1)
 
 
 def after_all(context):
