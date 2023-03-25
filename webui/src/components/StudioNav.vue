1fb68a9556b60b6259f647ff391754519bd4f99b
Sebastian Reimers <hallo@studio-link.de>
2023-03-25 21:36:24 +0100

    webui: improve roomnavbar

--- webui/src/components/StudioNav.vue
+++ webui/src/components/StudioNav.vue
@@ -4,25 +4,17 @@
     <div class="flex w-16 flex-col h-screen">
       <div class="flex min-h-0 flex-1 flex-col overflow-y-auto bg-sl-01dpa">
         <div class="flex-1">
-          <nav aria-label="Sidebar" class="flex flex-col items-center space-y-3 py-6">
-            <a href="#" class="relative block group -mt-2">
-              <div class="absolute flex items-center h-full -left-2">
-                <div class="h-10 w-1 transition-all duration-200 origin-left bg-sl-primary rounded-r"></div>
-              </div>
-              <div class="group-active:translate-y-px">
-                <div
-                  class="flex items-center justify-center w-12 h-12 transition-all duration-200 overflow-hidden rounded-2xl text-white hover:bg-sl-08dpa"
-                >
-                  <img class="h-10" src="https://www.podstock.de/assets/default_logo.jpg" alt="Logo" />
-                  <span class="sr-only">Studio Link Default</span>
-                </div>
-              </div>
-            </a>
-            <hr class="w-2/3 border-t-white/[.06] border-t-2 rounded mx-2" />
-            <a v-for="room in Users.rooms.value" :key="room.name" :href="room.url" class="relative block group">
+          <nav aria-label="Sidebar" class="flex flex-col items-center space-y-3 py-4">
+            <a
+              v-for="room in Users.rooms.value?.reverse()"
+              :key="room.name"
+              :href="room.url"
+              class="relative block group"
+            >
               <div class="absolute flex items-center h-full -left-2 -top-2">
                 <div
-                  class="h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100 w-1 transition-all duration-200 origin-left bg-gray-100 rounded-r"
+                  :class="[room.url != path ? 'scale-0 opacity-0' : '']" 
+                  class="h-5 group-hover:opacity-100 group-hover:scale-100 w-1 transition-all duration-200 origin-left bg-gray-100 rounded-r"
                 ></div>
               </div>
               <div class="group-active:translate-y-px">
@@ -41,8 +33,8 @@
                   </span>
                 </div>
               </div>
-              <div class="w-12 text-gray-200 text-xs mt-1 overflow-hidden whitespace-nowrap text-clip">
-                {{room.name}}
+              <div class="w-12 text-center text-gray-200 text-xs mt-1 overflow-hidden whitespace-nowrap text-clip">
+                {{ room.name }}
               </div>
             </a>
           </nav>
@@ -54,4 +46,8 @@
 
 <script setup lang="ts">
 import { Users } from '../ws/users'
+import {ref} from 'vue'
+
+const path=ref(location.pathname)
+
 </script>
