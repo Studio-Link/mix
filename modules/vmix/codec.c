18027c0f63ad2f4a5841cf0028d223981516c97e
Sebastian Reimers <hallo@studio-link.de>
2023-06-22 16:43:19 +0200

    improve vmix codec handling (WIP)

--- modules/vmix/codec.c
+++ modules/vmix/codec.c
@@ -2,40 +2,98 @@
 #include <mix.h>
 #include "vmix.h"
 
-
+/* H264 only */
 static videnc_encode_h *ench;
 static videnc_update_h *encupdh;
-
 static viddec_decode_h *dech;
 static viddec_update_h *decupdh;
+
+struct videnc_state {
+	videnc_packet_h *pkth;
+	const struct video *vid;
+	void *vesp; /* Sub-Codec state */
+};
+
+struct viddec_state {
+	const struct video *vid;
+	void *vdsp; /* Sub-Codec state */
+};
+
+
+static void videnc_deref(void *arg)
+{
+	struct videnc_state *ves = arg;
+
+	mem_deref(ves->vesp);
+}
+
+
+static void viddec_deref(void *arg)
+{
+	struct viddec_state *vds = arg;
+
+	mem_deref(vds->vdsp);
+}
 
 
 static int enc_update(struct videnc_state **vesp, const struct vidcodec *vc,
 		      struct videnc_param *prm, const char *fmtp,
-		      videnc_packet_h *pkth, void *arg)
+		      videnc_packet_h *pkth, const struct video *vid)
 {
-	return encupdh(vesp, vc, prm, fmtp, pkth, arg);
-}
+	struct videnc_state *ves;
 
+	ves = mem_zalloc(sizeof(struct videnc_state), videnc_deref);
+	if (!ves)
+		return ENOMEM;
 
-static int dec_update(struct viddec_state **vdsp, const struct vidcodec *vc,
-		      const char *fmtp)
-{
-	return decupdh(vdsp, vc, fmtp);
+	ves->vid = vid;
+
+	*vesp = ves;
+
+	return encupdh((struct videnc_state **)&ves->vesp, vc, prm, fmtp, pkth,
+		       vid);
 }
 
 
 static int encode(struct videnc_state *ves, bool update,
 		  const struct vidframe *frame, uint64_t timestamp)
 {
-	return ench(ves, update, frame, timestamp);
+	/* return ves->pkth(); */
+
+#if 0
+typedef int (videnc_packet_h)(bool marker, uint64_t rtp_ts,
+			      const uint8_t *hdr, size_t hdr_len,
+			      const uint8_t *pld, size_t pld_len,
+			      void *arg);
+#endif
+
+	return ench(ves->vesp, update, frame, timestamp);
+}
+
+
+static int dec_update(struct viddec_state **vdsp, const struct vidcodec *vc,
+		      const char *fmtp, const struct video *vid)
+{
+	struct viddec_state *vds;
+
+	vds = mem_zalloc(sizeof(struct viddec_state), viddec_deref);
+	if (!vds)
+		return ENOMEM;
+
+	vds->vid = vid;
+
+	*vdsp = vds;
+
+	return decupdh((struct viddec_state **)&vds->vdsp, vc, fmtp, vid);
 }
 
 
 static int decode(struct viddec_state *vds, struct vidframe *frame,
-		  bool *intra, bool marker, uint16_t seq, struct mbuf *mb)
+		  bool *intra, bool marker, uint16_t seq, uint64_t ts,
+		  struct mbuf *mb)
 {
-	return dech(vds, frame, intra, marker, seq, mb);
+
+	return dech(vds->vdsp, frame, intra, marker, seq, ts, mb);
 }
 
 
@@ -47,6 +105,7 @@ static struct vidcodec h264_0 = {
 	.decupdh = dec_update,
 	.dech	 = decode,
 };
+
 
 static struct vidcodec h264_1 = {
 	.name	 = "H264",
