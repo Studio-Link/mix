1ac256bf309b1d88ff6bcf8a1d749f3c4b238683
Sebastian Reimers <hallo@studio-link.de>
2023-07-10 07:36:32 +0200

    webui: add source number id

--- webui/src/components/WebrtcVideo.vue
+++ webui/src/components/WebrtcVideo.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex">
     <div v-show="Webrtc.state.value >= WebrtcState.Listening" class="w-1/3 mr-2">
-      <div v-for="(item, index) in sources" class="h-24 border-black border my-2">
-        <video :id="'source' + index" playsinline autoplay muted preload="none"></video>
+      <div v-for="(item) in sources" class="h-24 border-black border my-2">
+        <video :id="'source' + item.id" playsinline autoplay muted preload="none"></video>
       </div>
     </div>
 
