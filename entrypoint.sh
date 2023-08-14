60fcced655e7b4bf3a5724fa54ac72b6b342c1f7
Sebastian Reimers <hallo@studio-link.de>
2023-08-14 11:22:59 +0200

    docker: add Dockerfile and entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$1" = 'slmix' ]; then
+    if [[ -z $TOKENHOST ]]; then
+        echo "No TOKENHOST env!"
+        exit 1
+    fi
+    if [[ -z $TOKENDOWNLOAD ]]; then
+        echo "No TOKENDOWNLOAD env!"
+        exit 1
+    fi
+    if [[ -z $TOKENGUEST ]]; then
+        echo "No TOKENGUEST env!"
+        exit 1
+    fi
+    if [[ -z $TOKENAPI ]]; then
+        echo "No TOKENAPI env!"
+        exit 1
+    fi
+    cat > /opt/mix/config <<EOF
+mix_room                MixRoom
+mix_url                 /
+mix_token_host          $TOKENHOST # can start record
+mix_token_download      $TOKENDOWNLOAD # protected download folder 
+mix_token_guests        $TOKENGUEST # invite url 
+mix_token_api           $TOKENAPI # api token
+#mix_path               /opt/slmix/ 
+EOF
+    /opt/mix/build/slmix -c /opt/mix/config "$@"
+else
+    exec "$@"
+fi
