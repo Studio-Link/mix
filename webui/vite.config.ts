faf0a2c909ae2e873b1e6c18321ade03f3b264dc
Sebastian Reimers <hallo@studio-link.de>
2025-01-29 20:58:03 +0100

    refactor(webui): use vite proxy (/ws and /api)
    This is needed for cookie auth

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -11,4 +11,10 @@ export default defineConfig({
     define: {
         APP_VERSION: JSON.stringify("v1.0.0-beta-" + commitHash)
     },
+    server: {
+        proxy: {
+            '/api': { target: 'http://127.0.0.1:9999' },
+            '/ws': { target: 'ws://127.0.0.1:9999' },
+        }
+    }
 })
