e28803d3d783d1689161fc0757070e1b869f597c
Sebastian Reimers <hallo@studio-link.de>
2025-09-20 20:55:10 +0200

    sip: fix session hangup handling

--- src/sip.c
+++ src/sip.c
@@ -1,7 +1,27 @@
-#include <assert.h>
 #include <mix.h>
 
 static struct ua *sip_ua;
+static struct list sip_sessl = LIST_INIT;
+
+struct sip_sess_e {
+	struct le le;
+	struct call *call;
+	struct session *sess;
+};
+
+
+static void destruct_sess_e(void *arg)
+{
+	struct sip_sess_e *e = arg;
+
+	char *json = NULL;
+	user_event_json(&json, USER_DELETED, e->sess);
+	sl_ws_send_event(e->sess, json);
+	mem_deref(json);
+
+	list_unlink(&e->le);
+	mem_deref(e->sess);
+}
 
 
 static void ua_event_handler(enum bevent_ev ev, struct bevent *event,
@@ -10,6 +30,8 @@ static void ua_event_handler(enum bevent_ev ev, struct bevent *event,
 	struct mix *mix		  = arg;
 	struct call *call	  = bevent_get_call(event);
 	const struct sip_msg *msg = bevent_get_msg(event);
+	struct sip_sess_e *e	  = NULL;
+	int err			  = 0;
 
 	switch (ev) {
 
@@ -28,8 +50,21 @@ static void ua_event_handler(enum bevent_ev ev, struct bevent *event,
 
 		pl_set_str(&peer_pl, peer);
 
-		slmix_session_alloc(&sess, mix, NULL, NULL, &peer_pl, false,
-				    true);
+		err = slmix_session_alloc(&sess, mix, NULL, NULL, &peer_pl,
+					  false, true);
+		if (err)
+			goto hangup;
+
+		e = mem_zalloc(sizeof(struct sip_sess_e), destruct_sess_e);
+		if (!e) {
+			mem_deref(sess);
+			goto hangup;
+		}
+
+		e->call = call;
+		e->sess = sess;
+
+		list_append(&sip_sessl, &e->le, e);
 
 		pl_strcpy(&peer_pl, sess->user->id, sizeof(sess->user->id));
 
@@ -50,18 +85,34 @@ static void ua_event_handler(enum bevent_ev ev, struct bevent *event,
 
 		sess->user->pidx = slmix_disp_enable(mix, peer, true);
 
+
 		slmix_source_append_all(mix, call, peer);
 
 		break;
 
 	case BEVENT_CALL_CLOSED: {
 		slmix_source_deref(mix, call, NULL);
+		struct le *le;
+		struct le *le_tmp;
+		LIST_FOREACH_SAFE(&sip_sessl, le, le_tmp)
+		{
+			e = le->data;
+
+			if (e->call != call)
+				continue;
+
+			mem_deref(e);
+		}
 
 		break;
 	}
 	default:
 		break;
 	}
+
+	return;
+hangup:
+	call_hangup(call, 500, "Server Error");
 }
 
 
